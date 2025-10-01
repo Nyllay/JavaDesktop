9a1f3e69a1149e68cf7ea8a116988ec0a3c8e4b7
nyllay <nyllay>
2025-09-30 21:42:02 -0400

    mais

---
+++ src/main/java/com/grafico/Main.java
@@ -0,0 +1,9 @@
+package com.grafico;
+
+import javafx.application.Application;
+
+public class Main extends javafx.application.Application {
+     public static void main(String[] args) {
+         launch(args);
+     }
+}
