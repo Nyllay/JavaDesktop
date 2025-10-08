6e5763e4409bfe36f2f76b3313b01f95916e2439
nyllay <nyllay>
2025-10-07 21:46:45 -0400

    lala

--- grafico/src/main/java/com/interfase/grafico/controllers/MainController.java
+++ grafico/src/main/java/com/interfase/grafico/controllers/MainController.java
@@ -1,10 +1,17 @@
 package com.interfase.grafico.controllers;
 
+import javax.swing.Action;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private 
+    private Label label;
+
+    public void handleButtonClick(ActionEvent actionEvent){
+        label.setText("Botão Clicado!");
+    }
     
 }
