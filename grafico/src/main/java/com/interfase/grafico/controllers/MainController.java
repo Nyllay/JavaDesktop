be6859c76e2b3fac6ee580ac686084d3771b5b10
nyllay <nyllay>
2025-10-08 21:28:45 -0400

    lala

--- grafico/src/main/java/com/interfase/grafico/controllers/MainController.java
+++ grafico/src/main/java/com/interfase/grafico/controllers/MainController.java
@@ -1,17 +1,47 @@
 package com.interfase.grafico.controllers;
 
 import javax.swing.Action;
-
+import javafx.scene.control.Alert;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
-    private Label label;
+    private TextField nomeField;
 
-    public void handleButtonClick(ActionEvent actionEvent){
-        label.setText("Botão Clicado!");
+    @FXML
+    private TextField idadeField;
+
+    @FXML
+    private TextField emailField;
+
+    public void handleEnviar(ActionEvent actionEvent){
+
+        String nome = nomeField.getText();
+        String idade = idadeField.getText();
+        String email = emailField.getText();
+
+        if (idade == null || idade.trim().isEmpty()) {
+            showAlert("Por Favor, coloque a sua idade");   
+        }
+
+        if (email == null || email.trim().isEmpty()) {
+            showAlert("Por Favor, coloque o seu email");   
+        }
+        
+        if (nome == null || nome.trim().isEmpty()) {
+            showAlert("Por Favor, coloque o seu nome");   
+        }     
+
     }
-    
+    public void showAlert(String alerta) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Erro na valiação");
+            alert.setHeaderText(null);
+            alert.setContentText(alerta);
+            alert.showAndWait();       
+    }
 }
